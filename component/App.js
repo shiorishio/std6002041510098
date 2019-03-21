1e3fc0ebd7b7a9be5a13a41a429b941bc24e99dd
Shio <shio@Shio-3.local>
2019-03-21 13:28:44 +0700

    initial commit

---
+++ component/App.js
@@ -0,0 +1,18 @@
+// import library
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// write component
+class App extends React.Component {
+    render() {
+        return (
+            //component about react native
+            <View style={{ marginTop: 50, padding: 30 }}>
+                <Text>Hello Shiori</Text>
+            </View>
+        );
+    }
+}
+
+// export 
+export default App;

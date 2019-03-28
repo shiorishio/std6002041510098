5078192a74be478e6d01ef37961394fbfb4f4f64
Shio <shio@Shio-3.local>
2019-03-28 14:21:52 +0700

    using Header component on App

--- component/App.js
+++ component/App.js
@@ -1,6 +1,7 @@
 // import library
 import React from 'react';
 import { View, Text } from 'react-native';
+import Header from './Header'
 
 // write component
 class App extends React.Component {
@@ -8,6 +9,7 @@ class App extends React.Component {
         return (
             //component about react native
             <View style={{ marginTop: 50, padding: 30 }}>
+                <Header title="Albums" />
                 <Text>Hello Shiori</Text>
             </View>
         );

e2f21031be4cc5fa747cc671eea934813ca9678d
Shio <shio@Shio-3.local>
2019-03-28 14:53:51 +0700

    update

--- component/App.js
+++ component/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     render() {
         return (
             //component about react native
-            <View style={{ marginTop: 50, padding: 30 }}>
+            < View >
                 <Header title="Albums" />
                 <Text>Hello Shiori</Text>
             </View>
